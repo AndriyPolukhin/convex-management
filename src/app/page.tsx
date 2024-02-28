68f730213e87d724e767dce1f6c0dc311811a310
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-02-28 18:08:13 +0200

    Inititated convex with clerk auth

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { SignInButton, SignOutButton, auth } from '@clerk/nextjs'
+
+export default function Home() {
+	const { userId } = auth()
+
+	return (
+		<main className=''>{userId ? <SignOutButton /> : <SignInButton />}</main>
+	)
+}
