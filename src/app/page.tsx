cba21e7885410b312a184c8e77d0498c6e7de611
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-02-29 23:41:22 +0200

    Testing out the mutation setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
 'use client'
 
 import { SignInButton, SignOutButton, useSession } from '@clerk/nextjs'
+import { api } from '../../convex/_generated/api'
+import { useMutation } from 'convex/react'
 
 export default function Home() {
 	const { isSignedIn } = useSession()
 
+	const createThumbnail = useMutation(api.thumbnails.createThumbnail)
+
 	return (
 		<main className=''>
 			{isSignedIn ? <SignOutButton /> : <SignInButton />}
+
+			{isSignedIn && (
+				<form
+					onSubmit={async (e) => {
+						e.preventDefault()
+						const form = e.target as HTMLFormElement
+						const formData = new FormData(e.currentTarget)
+						const title = formData.get('title') as string
+						// Pass to convex mutation
+						await createThumbnail({ title })
+						form.reset()
+					}}
+				>
+					<label>Title</label>
+					<input name='title' className='text-black' /> <button>Create</button>
+				</form>
+			)}
 		</main>
 	)
 }
