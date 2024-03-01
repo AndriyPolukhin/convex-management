0c3e6ccdc846a409e3426abc2dd5e120f82016d0
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-03-01 23:01:08 +0200

    Added userId to the @fn handler

--- convex/thumbnails.ts
+++ convex/thumbnails.ts
@@ -6,8 +6,15 @@ export const createThumbnail = mutation({
 		title: v.string(),
 	},
 	handler: async (ctx, args) => {
+		const user = await ctx.auth.getUserIdentity()
+
+		if (!user) {
+			throw new Error('you must be logged in to create a thumbnail')
+		}
+
 		await ctx.db.insert('thumbnails', {
 			title: args.title,
+			userId: user.subject,
 		})
 	},
 })
