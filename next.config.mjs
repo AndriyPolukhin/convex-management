b61541cfd8a7caad89feda6937962852822f2170
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-03-02 18:21:14 +0200

    updated packages with ui's

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				hostname: 'capable-malamute-771.convex.cloud',
+			},
+		],
+	},
+}
 
-export default nextConfig;
+export default nextConfig
