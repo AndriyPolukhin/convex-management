3872919094d442f7c592007f19f730c207ba8aca
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-03-03 14:16:01 +0200

    Clean up the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,34 +10,5 @@ export default function Home() {
 	const createThumbnail = useMutation(api.thumbnails.createThumbnail)
 	const thumbnails = useQuery(api.thumbnails.getThumbnailsForUser)
 
-	return (
-		<main className=''>
-			{isSignedIn && (
-				<form
-				// onSubmit={async (e) => {
-				// 	e.preventDefault()
-				// 	const form = e.target as HTMLFormElement
-				// 	const formData = new FormData(e.currentTarget)
-				// 	const title = formData.get('title') as string
-				// 	// Pass to convex mutation
-				// 	await createThumbnail({ title })
-				// 	form.reset()
-				// }}
-				>
-					<label>Title</label>
-					<input name='title' className='text-black' />
-					<button>Create</button>
-				</form>
-			)}
-
-			{thumbnails &&
-				thumbnails.map((thumbnail) => {
-					return (
-						<div key={`${thumbnail.title}-${thumbnail._id}`}>
-							{thumbnail.title}
-						</div>
-					)
-				})}
-		</main>
-	)
+	return <main className=''></main>
 }
