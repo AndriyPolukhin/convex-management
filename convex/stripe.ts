a9ca10702e546b79927cf2180cf1e1a9f50563ba
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-03-04 23:34:31 +0200

    Added the stripe pay @fn

--- convex/stripe.ts
+++ convex/stripe.ts
@@ -2,12 +2,45 @@
 
 import { v } from 'convex/values'
 import Stripe from 'stripe'
-import { internalAction } from './_generated/server'
+import { action, internalAction } from './_generated/server'
 import { internal } from './_generated/api'
 
 type Metadata = {
 	userId: string
 }
+
+export const pay = action({
+	args: {},
+	handler: async (ctx) => {
+		const user = await ctx.auth.getUserIdentity()
+		if (!user) {
+			throw new Error('you must be logged in to subscribe')
+		}
+
+		if (!user.emailVerified) {
+			throw new Error('you must have a verified email to subscribe')
+		}
+
+		const domain = process.env.HOSTING_URL ?? 'http://localhost:3000'
+		const stripe = new Stripe(process.env.STRIPE_KEY!, {
+			apiVersion: '2023-10-16',
+		})
+
+		const session = await stripe.checkout.sessions.create({
+			line_items: [{ price: process.env.PRICE_ID!, quantity: 1 }],
+			customer_email: user.email,
+			metadata: {
+				userId: user.subject,
+			},
+			mode: 'payment',
+			success_url: `${domain}`,
+			cancel_url: `${domain}`,
+			automatic_tax: { enabled: true },
+		})
+
+		return session.url
+	},
+})
 
 export const fulfill = internalAction({
 	args: { signature: v.string(), payload: v.string() },
